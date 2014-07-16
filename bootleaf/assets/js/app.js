bafa40ed6d5557adda9dfdba3164398e9090dfea
maptastik <ryancooper729@gmail.com>
2014-07-16 11:09:49 -0400

    trying something with bike

--- bootleaf/assets/js/app.js
+++ bootleaf/assets/js/app.js
@@ -88,121 +88,15 @@ $.getJSON("data/hike.geojson", function (data) {
   hike.addData(data);
 });
 
-// var subwayLines = L.geoJson(null, {
-//   style: function (feature) {
-//     if (feature.properties.route_id === "1" || feature.properties.route_id === "2" || feature.properties.route_id === "3") {
-//       return {
-//         color: "#ff3135",
-//         weight: 3,
-//         opacity: 1
-//       };
-//     }
-//     if (feature.properties.route_id === "4" || feature.properties.route_id === "5" || feature.properties.route_id === "6") {
-//       return {
-//         color: "#009b2e",
-//         weight: 3,
-//         opacity: 1
-//       };
-//     }
-//     if (feature.properties.route_id === "7") {
-//       return {
-//         color: "#ce06cb",
-//         weight: 3,
-//         opacity: 1
-//       };
-//     }
-//     if (feature.properties.route_id === "A" || feature.properties.route_id === "C" || feature.properties.route_id === "E" || feature.properties.route_id === "SI" || feature.properties.route_id === "H") {
-//       return {
-//         color: "#fd9a00",
-//         weight: 3,
-//         opacity: 1
-//       };
-//     }
-//     if (feature.properties.route_id === "Air") {
-//       return {
-//         color: "#ffff00",
-//         weight: 3,
-//         opacity: 1
-//       };
-//     }
-//     if (feature.properties.route_id === "B" || feature.properties.route_id === "D" || feature.properties.route_id === "F" || feature.properties.route_id === "M") {
-//       return {
-//         color: "#ffff00",
-//         weight: 3,
-//         opacity: 1
-//       };
-//     }
-//     if (feature.properties.route_id === "G") {
-//       return {
-//         color: "#9ace00",
-//         weight: 3,
-//         opacity: 1
-//       };
-//     }
-//     if (feature.properties.route_id === "FS" || feature.properties.route_id === "GS") {
-//       return {
-//         color: "#6e6e6e",
-//         weight: 3,
-//         opacity: 1
-//       };
-//     }
-//     if (feature.properties.route_id === "J" || feature.properties.route_id === "Z") {
-//       return {
-//         color: "#976900",
-//         weight: 3,
-//         opacity: 1
-//       };
-//     }
-//     if (feature.properties.route_id === "L") {
-//       return {
-//         color: "#969696",
-//         weight: 3,
-//         opacity: 1
-//       };
-//     }
-//     if (feature.properties.route_id === "N" || feature.properties.route_id === "Q" || feature.properties.route_id === "R") {
-//       return {
-//         color: "#ffff00",
-//         weight: 3,
-//         opacity: 1
-//       };
-//     }
-//   },
-//   onEachFeature: function (feature, layer) {
-//     if (feature.properties) {
-//       var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Division</th><td>" + feature.properties.Division + "</td></tr>" + "<tr><th>Line</th><td>" + feature.properties.Line + "</td></tr>" + "<table>";
-//       layer.on({
-//         click: function (e) {
-//           $("#feature-title").html(feature.properties.Line);
-//           $("#feature-info").html(content);
-//           $("#featureModal").modal("show");
-//           highlight.clearLayers().addLayer(L.circleMarker([e.latlng.lat, e.latlng.lng], {
-//             stroke: false,
-//             fillColor: "#00FFFF",
-//             fillOpacity: 0.7,
-//             radius: 10
-//           }));
-//         }
-//       });
-//     }
-//     layer.on({
-//       mouseover: function (e) {
-//         var layer = e.target;
-//         layer.setStyle({
-//           weight: 3,
-//           color: "#00FFFF",
-//           opacity: 1
-//         });
-//         if (!L.Browser.ie && !L.Browser.opera) {
-//           layer.bringToFront();
-//         }
-//       },
-//       mouseout: function (e) {
-//         subwayLines.resetStyle(e.target);
-//       }
-//     });
-//   }
-// });
+var bike = L.geoJson(null, {
+  style: function (feature) {
+      return {
+        color: "#ffff00",
+        weight: 3,
+        opacity: 1
+      };
+  },
+});
 // var bike = L.geoJson(null, {
 //   style: function (feature) {
 //     return {
