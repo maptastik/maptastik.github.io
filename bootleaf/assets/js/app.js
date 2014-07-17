1873ac88887c48db36a0814e1412b102cb3df524
maptastik <ryancooper729@gmail.com>
2014-07-17 15:00:21 -0400

    actually try to add trail heads icon to map

--- bootleaf/assets/js/app.js
+++ bootleaf/assets/js/app.js
@@ -376,7 +376,7 @@ var baseLayers = {
 
 var groupedOverlays = {
   "Points of Interest": {
-    "<img src='assets/img/theater.png' width='24' height='28'>&nbsp;Trail Heads": trailHeadsLayer,
+    "<img src='assets/img/trailHead-icon-12.svg' width='24' height='28'>&nbsp;Trail Heads": trailHeadsLayer,
     "<img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums": museumLayer
   },
   "Trails": {
