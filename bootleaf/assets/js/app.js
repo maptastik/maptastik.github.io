2cee1b254f0cad0cc0304934287368bf3e288988
maptastik <ryancooper729@gmail.com>
2014-07-28 09:55:16 -0400

    try again

--- bootleaf/assets/js/app.js
+++ bootleaf/assets/js/app.js
@@ -44,7 +44,8 @@ function sidebarClick(id) {
 }
 
 /* Basemap Layers */
-var mbSTD = L.tileLayer("https://{s}.tiles.mapbox.com/v4/maptastik.j354k5k8/{z}/{x}/{y}.png", {
+// https://a.tiles.mapbox.com/v4/maptastik.j354k5k8/page.html?access_token=pk.eyJ1IjoibWFwdGFzdGlrIiwiYSI6IjNPMkREV1kifQ.2KGPFZD0QaGfvYzXYotTXQ#15/38.2082/-84.5610
+var mbSTD = L.tileLayer("https://{s}.tiles.mapbox.com/v4/maptastik.j354k5k8/page.html?access_token=pk.eyJ1IjoibWFwdGFzdGlrIiwiYSI6IjNPMkREV1kifQ.2KGPFZD0QaGfvYzXYotTXQ#{z}/{x}/{y}.png", {
   minZoom: 10,
   maxZoom: 16,
   // subdomains: ["a", "b", "c"],
